cb8573c45ff5c0caa296f84bea153b86e04cdb1d
DavidUdala <david_udala@hotmail.com>
2024-07-07 14:25:25 -0300

    removido router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
@@ -25,7 +24,6 @@ import { CardPresenteComponent } from './componentes/card-presente/card-presente
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     NgbModule
   ],
   providers: [],
